eb59b5e730256163f526bba76b3b018894b65412
ThijsBrekhof <thijs.brekhof@gmail.com>
2022-03-28 14:15:46 +0200

    Thijs' first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Authors: Thijs Brekhof, Cain Weideman, Twan Heeres
+# The main python file we will use to classift musical genres
+
+
+
+
+def main():
+    print("Test 123")
+
+
+
+
+if __name__ == "__main__":
+    main()
