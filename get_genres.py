0942c5b0ee42086dc893d308fe64117db2dd8716
thijsbrekhof1 <60843602+thijsbrekhof1@users.noreply.github.com>
2022-03-30 22:21:40 +0200

    Fixed get_genres.py and added the resulting dictionary in a pickle file

--- get_genres.py
+++ get_genres.py
@@ -1,28 +1,42 @@
 # Authors: Thijs Brekhof, Cain Weideman, Twan Heeres
-# The main python file we will use to classify musical genres
-import eyed3
+# In this file we will save all known genres to a pickle (dictionary) file
+import pandas as pd
 import os
 import pickle
 
 
-def main():
-
+def get_our_tracks():
     directory = "fma_small/"
 
-    genre_dict = {}
-
-    eyed3.log.setLevel("ERROR")
-
+    our_files = []
     for root, subdirectories, files in os.walk(directory):
-        for subdirectory in subdirectories:
-            new_path = os.path.join(root, subdirectory)
-            file_list = os.listdir(new_path)
-            for i in file_list:
-                file_path = new_path + '/' + i
-                audio = eyed3.load(file_path)
-                if audio.tag.genre:
-                    genre_dict[i] = audio.tag.genre.name
+        # print(files)
+        for file in files:
+            our_files.append(file[:-4])
+    return our_files[2:]
 
+
+def main():
+    # Opening csv file
+    df = pd.read_csv("tracks.csv", low_memory=False)
+    # Fixing messy data and adding padding
+    df = df.rename(columns=df.iloc[0])
+    df = df.rename(columns={df.columns[0]: 'track_id'})
+    df = df.drop(df.index[0:2])
+    df.drop(df.columns.difference(["track_id", "genre_top"]), 1, inplace=True)
+
+    # Padding the track ids
+    df['track_id'] = df['track_id'].apply(lambda x: '{0:0>6}'.format(x))
+    # Getting all tracks we are using in the fma_small dataset
+    tracks = get_our_tracks()
+
+    # Getting genres per track
+    genre_dict = {}
+    for _, row in df.iterrows():
+        if row["track_id"] in tracks:
+            genre_dict[row["track_id"]] = row["genre_top"]
+
+    # Writing the dict to a pickle file
     with open('genre_dict.pickle', 'wb') as f:
         pickle.dump(genre_dict, f)
 
