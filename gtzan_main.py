23b1303c1e94b3187e25ff12c9cf6a9ec357c621
CainWeideman <c.weideman@student.rug.nl>
2022-04-06 21:36:47 +0200

    Can now add summations

--- gtzan_main.py
+++ gtzan_main.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from sklearn.svm import SVC
 import pickle
 from itertools import repeat
@@ -5,23 +6,39 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import collections
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 def get_data():
     # MFCC dict
-    infile = open("big_wav_mfcc_dict.pickle", 'rb')
+    infile = open("sum_gtzan_mfcc_dict.pickle", 'rb')
     mfcc_dict = pickle.load(infile)
     infile.close()
+    #for i in mfcc_dict:
+        #print(np.array(mfcc_dict[i]).shape)
+    #first = list(mfcc_dict.values())[0]
+    #print(first)
     mfccs = []
     for i in mfcc_dict:
         mfccs.append(mfcc_dict[i])
 
+    #mfccs = np.array(mfccs)
+    #nsamples, nx, ny = mfccs.shape
+    #mfccs = mfccs.reshape((nsamples, nx * ny))
     labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
     label_list = []
     for i in labels:
         label_list.extend(repeat(i, 100))
 
+    counter = collections.Counter(label_list)
+    #for k, v in dict(counter).items():
+        #print(k, v)
     return label_list, mfccs
+
 
 def main():
     # Getting our training input/labels
@@ -30,16 +47,29 @@ def main():
     # Splitting the data in train/test (80/20)
     x_train, x_test, y_train, y_test = train_test_split(mfccs, labels, test_size=0.20, random_state=69)
 
-    # Kernel types: ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed
-    #svm = SVC(kernel="poly")
-    #svm.fit(x_train, y_train)
-    #scores = cross_val_score(svm, x_test, y_test, cv=5)
-    #predictions = svm.predict(x_test)
+    # Kernel types: ‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed‘
+    svm = SVC(kernel="linear")
+    svm.fit(x_train, y_train)
+    predictions = svm.predict(x_test)
 
-    neigh = KNeighborsClassifier(n_neighbors=10)
-    neigh.fit(x_train, y_train)
-    predictions = neigh.predict(x_test)
+    #neigh = KNeighborsClassifier(n_neighbors=50)
+    #neigh.fit(x_train, y_train)
+    #predictions = neigh.predict(x_test)
+
     print(accuracy_score(y_test, predictions))
+    matrix = confusion_matrix(y_test, predictions)
+    ax = sns.heatmap(matrix, annot=True, cmap='Blues')
+
+    ax.set_title('GTZAN SVM Linear\n\n')
+    ax.set_xlabel('\nPredicted Genre')
+    ax.set_ylabel('Actual Genre ')
+
+    ## Ticket labels - List must be in alphabetical order
+    ax.xaxis.set_ticklabels(['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'])
+    ax.yaxis.set_ticklabels(['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock'])
+
+    ## Display the visualization of the Confusion Matrix.
+    plt.show()
     #print(scores)
 
 
--- wav_to_mfcc.py
+++ wav_to_mfcc.py
@@ -1,5 +1,7 @@
 import scipy.io.wavfile as wav
 from python_speech_features import mfcc
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 import librosa
 
 import os
@@ -15,23 +17,22 @@ def main():
     mfcc_dict = {}
     directory = "wav-data/"
     file_list = os.listdir(directory)
-    count = 0
     for audio_file in file_list:
-        #print(audio_file)
         file_path = directory + audio_file
-        (rate, sig) = wav.read(file_path)
-        mfcc_feat = mfcc(sig, rate)
-        mfcc_feat = np.transpose(mfcc_feat)
-        #signal, sr = librosa.load(file_path)
-        #mfcc_feat = librosa.feature.mfcc(signal, n_mfcc=13, sr=sr)
+
+        signal, sr = librosa.load(file_path)
+        mfcc_feat = librosa.feature.mfcc(signal, n_mfcc=13, sr=sr)
+
         mfcc_list = []
         for i in mfcc_feat:
-            mean = np.mean(i)
-            mfcc_list.append(mean)
-        #print(len(mfcc_list))
+            #mean = np.mean(i)
+            #mfcc_list.append(mean)
+            sum = np.sum(i)
+            mfcc_list.append(sum)
 
         mfcc_dict[audio_file] = mfcc_list
-    with open('big_wav_mfcc_dict.pickle', 'wb') as f:
+
+    with open('sum_gtzan_mfcc_dict.pickle', 'wb') as f:
         pickle.dump(mfcc_dict, f)
 
 
